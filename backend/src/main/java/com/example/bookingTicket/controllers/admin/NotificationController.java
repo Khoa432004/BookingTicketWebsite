0721a7fdd4f5d76c82aa83fbfd5dc3d6b51e1e7c
nguyenvankhanh2010 <vkhanh1224@gmail.com>
2025-05-14 21:26:40 +0700

    add

--- backend/src/main/java/com/example/bookingTicket/controllers/admin/NotificationController.java
+++ backend/src/main/java/com/example/bookingTicket/controllers/admin/NotificationController.java
@@ -1,3 +1,5 @@
+package com.example.bookingTicket.controllers.admin;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
